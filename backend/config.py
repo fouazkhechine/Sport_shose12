812d3470a385fdd192f2c63e0821d87d2d2d09b4
Faouazkhechine <khechine.faouaz.fs@univ-boumerdes.dz>
2026-03-30 04:00:49 +0100

    Refactor DB_CONFIG to remove default values

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,10 @@
 import os
 
 DB_CONFIG = {
-    "host":     os.environ.get("DB_HOST", "localhost"),
+    "host":     os.environ.get("DB_HOST"),
     "port":     int(os.environ.get("DB_PORT", 3306)),
-    "user":     os.environ.get("DB_USER", "root"),
-    "password": os.environ.get("DB_PASSWORD", ""),
-    "database": os.environ.get("DB_NAME", "sport_shoes_catalog"),
+    "user":     os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_NAME"),
     "charset":  "utf8mb4",
 }
